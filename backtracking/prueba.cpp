079c3e6810faae5c1b1f5ab04977260bf9f557f1
Makusoku <marcosfrisancho123@gmail.com>
2023-07-24 15:30:36 -0500

    Clase 08

---
+++ backtracking/prueba.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+void solve(){
+
+    cout << "Backtracking";    
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0); 
+    ll t;
+
+    solve();
+    return 0;
+}
